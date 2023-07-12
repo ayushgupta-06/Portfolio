ca01ce843fa141acbd32a84626e2b001cc16e76d
ayushgupta-06 <ayushgupta2k7@gmail.com>
2023-07-12 12:46:58 +0530

    home page added

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,12 @@
 const hamburgerLines = document.querySelectorAll(".lines")
 const hamburger = document.querySelector(".hamburger")
 const header = document.querySelector("header")
-const navbar = document.querySelector(".navbar")
+const container = document.querySelector(".container")
 hamburgerLines.forEach(element => {
     element.addEventListener('click',hamburgerLinesClick)
 });
 function hamburgerLinesClick() {
     hamburger.classList.toggle('active')
     header.classList.toggle('header-active')
-    navbar.classList.toggle('nav-active')
+    container.classList.toggle('container-active')
 }
