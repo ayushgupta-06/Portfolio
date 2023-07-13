c1645f089a60c7e4af676ddb7bf1af5b615174ca
ayushgupta-06 <ayushgupta2k7@gmail.com>
2023-07-13 12:50:24 +0530

    about page added

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,7 @@ const hamburgerLines = document.querySelectorAll(".lines")
 const hamburger = document.querySelector(".hamburger")
 const header = document.querySelector("header")
 const container = document.querySelector(".container")
+const navbtn = document.querySelectorAll(".navbtn")
 hamburgerLines.forEach(element => {
     element.addEventListener('click',hamburgerLinesClick)
 });
@@ -10,4 +11,12 @@ function hamburgerLinesClick() {
     hamburger.classList.toggle('active')
     header.classList.toggle('header-active')
     container.classList.toggle('container-active')
+}
+navbtn.forEach(element => {
+    element.addEventListener('click',navbtnClick)
+});
+function navbtnClick(){
+    hamburger.classList.remove('active')
+    header.classList.remove('header-active')
+    container.classList.remove('container-active')
 }
